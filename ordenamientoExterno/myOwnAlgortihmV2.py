65ca63d508976ddd3272c3f4839ba6cd4f72b865
HectorRobles <robletes062901@gmail.com>
2020-11-26 21:31:46 -0600

    busqueda

--- ordenamientoExterno/myOwnAlgortihmV2.py
+++ ordenamientoExterno/myOwnAlgortihmV2.py
@@ -1,42 +1,50 @@
 #-*-coding:utf-8-*-
 import random
+import numpy as np
 
-def getPriorityNumbers(data):
-        middle = (len(data)//2)
-        maxData = data[-1]
-        minData = data[0]
-        middleData = data[middle]
-        return middle, maxData, minData, middleData
-
-def compareAndAppend(num, maxNum, minNum, middleNum, middle, data):
-    flag = False
-    if num > maxNum:
-        data.append(num)
-        flag = True
-    elif num < minNum:
-        data.insert(0, num)
-        flag = True
-    elif num == middleNum:
-        data.insert(middle + 1, num)
-        flag = True
-    elif not flag:
-        newData = data[:middle]
-        newMiddle, newMax, newMin, newMiddleData = getPriorityNumbers(newData)
-        return compareAndAppend(num, newMax, newMin, newMiddleData, newData)
-    elif not flag:
-        newData = data[:middle]
-        newMiddle, newMax, newMin, newMiddleData = getPriorityNumbers(newData)
-        return compareAndAppend(num, newMax, newMin, newMiddleData, newData)
+def reshapeData(data, targets):
+    for i in targets:
+        data.pop(i)
     return data
+
+def getLen(data):
+    count = 0
+    for i in data:
+        count += 1
+    return count
+
+def compareAndAppend(num, data):
+    flag = False
+    if getLen(data) > 1:
+        maxNum = data[-1]
+        minNum = data[0]
+        if num > maxNum:
+            flag = True
+            return len(data)
+        elif num < minNum:
+            flag = True
+            return 0
+        elif not flag:
+                middle = getLen(data)
+                if num > middle:
+                    return compareAndAppend(num, data[middle:-1])
+                elif num < middle:
+                    return compareAndAppend(num, data[0:middle])
+                else:
+                    return middle
+    else:
+        return getLen(data)
 
 def algorithm(data):
     count = 0
     dataSorted = []
     dataSorted.append(data[0])
-    for i in range(1, len(data)):
+    print(dataSorted)
+    while getLen(data) > 1:
         count += 1
-        middle, maxI, minI, middleI = getPriorityNumbers(data)
-        dataSorted = compareAndAppend(i, maxI, minI, middleI, middle, dataSorted)
+        data = reshapeData(data, [0])
+        pos = compareAndAppend(data[0], dataSorted)
+        dataSorted.insert(pos, data[0])
         print(dataSorted)
 
 def makeTestData(n):
