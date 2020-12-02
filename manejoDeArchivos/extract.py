fe02b0fe6977b5a6a8b121c6af687ea7f27c3508
HectorRobles <robletes062901@gmail.com>
2020-12-01 22:28:14 -0600

    busqueda

--- manejoDeArchivos/extract.py
+++ manejoDeArchivos/extract.py
@@ -1,56 +1,73 @@
+def cleanData(data):
+    data = data.replace(data[-1], '')
+    moreThan = data.find('>')
+    lessThan = data.find('</')
+    data = data[moreThan+1:lessThan]
+    return data
+
+def addToDicc(dicc, data):
+    if data in dicc:
+        dicc[data] += 1
+    else:
+        dicc[data] = 1
+    return dicc
+
+def getMaxConcurrency(dicc):
+    maxConc = max(dicc, key = dicc.get)
+    return maxConc
+
 def extractUserData():
     names = {}
     emails = {}
     passwords = {}
+    concurrencies = {}
+    genders = {}
+    shirtSizes = {}
     file = open('dataset.xml', 'r')
     line = 'algo'
+    i = 0
     while line != '':
         if line == '  <record>':
             name = file.readline()
             email = file.readline()
             password = file.readline()
-            name = name.replace(name[-1], '')
-            name = name.replace(' ', '')
-            name = name.replace('<first_name>', '')
-            name = name.replace('</first_name>', '')
-            if name in names:
-                names[name] += 1
-            else:
-                names[name] = 1
-            email = email.replace(email[-1], '')
-            email = email.replace(' ', '')
-            email = email.replace('<email>', '')
-            email = email.replace('</email>', '')
+            concurrency = file.readline()
+            gender = file.readline()
+            shirtSize = file.readline()
+            name = cleanData(name)
+            names = addToDicc(names, name)
+            email = cleanData(email)
             at = email.find('@')
             dot = email.find('.')
             email = email[at+1:dot]
-            if email in emails:
-                emails[email] += 1
-            else:
-                emails[email] = 1
-            password = password.replace(password[-1], '')
-            password = password.replace(' ', '')
-            password = password.replace('<password>', '')
-            password = password.replace('</password>', '')
-            if password in passwords:
-                passwords[password] += 1
-            else:
-                passwords[password] = 1
-        line = file.readline()
-        line = line.replace(line[-1], '')
+            emails = addToDicc(emails, email)
+            password = cleanData(password)
+            passwords = addToDicc(passwords, password)
+            concurrency = cleanData(concurrency)
+            concurrencies = addToDicc(concurrencies, concurrency)
+            gender = cleanData(gender)
+            genders = addToDicc(genders, gender)
+            shirtSize = cleanData(shirtSize)
+            shirtSizes = addToDicc(shirtSizes, shirtSize)
+        try:
+            line = file.readline()
+            line = line.replace(line[-1], '')
+        except:
+            break
     if file:
         file.close()
-    """
-    mayorNombre = max(names.keys())
-    mayorEmail = max(emails.keys())
-    mayorPassword = max(passwords.keys())
-    """
-    mayorNombre =max(names, key = names.get)
-    mayorEmail = max(emails, key = emails.get)
-    mayorPassword = max(passwords, key = passwords.get)
-    print(mayorNombre, ':', names.get(mayorNombre))
-    print(mayorEmail, ':', emails.get(mayorEmail))
-    print(mayorPassword, ':', passwords.get(mayorPassword))
+    moreFrequentlyName = getMaxConcurrency(names)
+    moreFrequentlyEmail = getMaxConcurrency(emails)
+    moreFrequentlyPassword = getMaxConcurrency(passwords)
+    moreFrequentlyConcurrencies = getMaxConcurrency(concurrencies)
+    moreFrequentlyGender = getMaxConcurrency(genders)
+    moreFrequentlyShirtSize = getMaxConcurrency(shirtSizes)
+    print(moreFrequentlyName, ':', names.get(moreFrequentlyName))
+    print(moreFrequentlyEmail, ':', emails.get(moreFrequentlyEmail))
+    print(moreFrequentlyPassword, ':', passwords.get(moreFrequentlyPassword))
+    print(moreFrequentlyConcurrencies, ':', concurrencies.get(moreFrequentlyConcurrencies))
+    print(moreFrequentlyGender, ':', genders.get(moreFrequentlyGender))
+    print(moreFrequentlyShirtSize, ':', shirtSizes.get(moreFrequentlyShirtSize))
 
 if __name__ == '__main__':
     extractUserData()
