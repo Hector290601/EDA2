1e4ccdcac321dfadb72b7a84685cbc1795a52217
HectorRobles <robletes062901@gmail.com>
2020-12-08 14:01:42 -0600

    busqueda

--- openmp/parallelVerify/main.c
+++ openmp/parallelVerify/main.c
@@ -8,6 +8,17 @@ int main(){
 	printf("Numero de hilos: %d", omp_get_max_threads());
 	omp_set_num_threads(procs);
 	printf("En paralelo: %d\n", omp_in_parallel());
+	int limit = (int) (procs/2);
+	printf("===CON HILOS LIMITAODS A %d===\n", limit);
+	#pragma omp parallel num_threads(limit)
+	{
+		int ths = omp_get_num_threads();
+		printf("Hilos: %d\n", ths);
+		int id = omp_get_thread_num();
+		printf("Hilo actual: %d\n", id);
+		printf("En paralelo: %d\n", omp_in_parallel());
+	}
+	printf("===SIN LIMITE DE HILOS===\n");
 	#pragma omp parallel
 	{
 		int ths = omp_get_num_threads();
@@ -18,3 +29,4 @@ int main(){
 	}
 	return 0;
 }
+
