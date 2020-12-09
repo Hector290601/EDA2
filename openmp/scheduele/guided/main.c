fc35d6f44aa5a00f8fa23f1e3ccb0d750124434a
HectorRobles <robletes062901@gmail.com>
2020-12-09 14:42:45 -0600

    busqueda

---
+++ openmp/scheduele/guided/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <unistd.h>
+
+#define THREADS 4
+#define N 8
+
+int main(void){
+	int i;
+	#pragma omp parallel for schedule(guided) num_threads(THREADS)
+		for(i = 0; i<N; i++){
+			sleep(i);
+			printf("Thread: %d, Itertion: %d.\n", omp_get_thread_num(), i);
+		}
+	return 0;
+}
